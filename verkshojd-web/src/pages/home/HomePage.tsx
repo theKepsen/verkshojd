6250a37343e584a218ce0231fe3d959b969de0b2
theKepsen <emil.h.andersson@gmail.com>
2026-03-14 00:04:17 +0100

    feat(home): restructure HomePage layout and integrate game data with CardLink components

--- verkshojd-web/src/pages/home/HomePage.tsx
+++ verkshojd-web/src/pages/home/HomePage.tsx
@@ -1,25 +1,47 @@
 //import { useMainState } from "../../providers/MainStateProvider/MainStateProvider";
 import { PopUpImage } from "../../components/PopUpImage/PopUpImage";
-import {
-  HomePageProvider,
-  useHomePageContext,
-} from "./provider/HomePageProvider";
+import { HomePageProvider } from "./provider/HomePageProvider";
 import img from "/verkshojden.png";
+import styles from "./HomePage.module.css";
+import { gamesData } from "../games/data/gameData";
+import { CardLink } from "../games/components/cardLink/CardLink";
 
 export const HomePage = () => {
-  const { text } = useHomePageContext();
-
   return (
     <HomePageProvider>
-      <h1>{text}</h1>
-      <PopUpImage
-        containerStyle={{ width: "500px", maxHeight: "70%", margin: "0 auto" }}
-        imageSrc={img}
-        altText="Verkshojd Logo"
-      />
-      <span className="veryImportantInformation">
-        This is some very important information that should be highlighted.
-      </span>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.fanty}>
+            <PopUpImage
+              containerStyle={{
+                width: "500px",
+                maxHeight: "70%",
+                margin: "0",
+                marginTop: "48px",
+                marginLeft: "25%",
+              }}
+              imageSrc={img}
+              altText="Verkshojd Logo"
+            />
+          </div>
+          <div className={styles.info}>
+            {gamesData.map((game) => (
+              <CardLink
+                key={game.title}
+                title={game.title}
+                description={game.description}
+                link={game.link}
+                imageSrc={game.imageSrc}
+                state={game.state}
+              />
+            ))}
+            <span>Hey! Check out this cool game!</span>
+          </div>
+        </div>
+        <span className="veryImportantInformation">
+          This is some very important information that should be highlighted.
+        </span>
+      </div>
     </HomePageProvider>
   );
 };
